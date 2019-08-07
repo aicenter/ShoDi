2890679282a1332cbafb71f28c612b169c446030
Cvach, Michal, Bc <cvachmic@fel.cvut.cz>
2019-08-07 13:28:57 +0200

    Changed TNRGLoader to reflect changes to TNRGPreprocessor. It now loads TNR data-structures in the binary format instead of the more space consuming plain text format.

--- src/GraphBuilding/Loaders/TNRGLoader.cpp
+++ src/GraphBuilding/Loaders/TNRGLoader.cpp
@@ -17,7 +17,7 @@ TNRGLoader::TNRGLoader(string inputFile) : inputFile(inputFile) {
 //______________________________________________________________________________________________________________________
 TransitNodeRoutingGraph * TNRGLoader::loadTNR() {
     ifstream input;
-    input.open(this->inputFile);
+    input.open(this->inputFile, ios::binary);
     if( ! input.is_open() ) {
         printf("Couldn't open file '%s'!", this->inputFile.c_str());
     }
@@ -49,7 +49,10 @@ TransitNodeRoutingGraph * TNRGLoader::loadTNR() {
 //______________________________________________________________________________________________________________________
 void TNRGLoader::parseFirstLine(ifstream & input, unsigned int & nodes, unsigned int & edges, unsigned int & tnodesAmount) {
     char c1, c2, c3, c4;
-    input >> c1 >> c2 >> c3 >> c4;
+    input.read (&c1, sizeof(c1));
+    input.read (&c1, sizeof(c2));
+    input.read (&c1, sizeof(c3));
+    input.read (&c1, sizeof(c4));
     if (c1 != 'T' || c2 != 'N' || c3 != 'R' || c4 != 'G') {
         cout << "The input file is missing the expected header!" << endl
              << "Transit Node Routing Graph file should begin with the string 'TNRG'." << endl
@@ -57,15 +60,22 @@ void TNRGLoader::parseFirstLine(ifstream & input, unsigned int & nodes, unsigned
              << "The loading will proceed but the loaded graph might be corrupted." << endl;
     }
 
-    input >> nodes >> edges >> tnodesAmount;
+    input.read ((char *) &nodes, sizeof(nodes));
+    input.read ((char *) &edges, sizeof(edges));
+    input.read ((char *) &tnodesAmount, sizeof(tnodesAmount));
 }
 
 //______________________________________________________________________________________________________________________
 void TNRGLoader::parseEdges(ifstream & input, TransitNodeRoutingGraph & graph, unsigned int edges) {
     unsigned int from, to, weight;
     bool forward, backward;
+
     for(unsigned int i = 0; i < edges; i++) {
-        input >> from >> to >> weight >> forward >> backward;
+        input.read ((char *) &from, sizeof(from));
+        input.read ((char *) &to, sizeof(to));
+        input.read ((char *) &weight, sizeof(weight));
+        input.read ((char *) &forward, sizeof(forward));
+        input.read ((char *) &backward, sizeof(backward));
         graph.addEdge(from, to, weight, forward, backward);
     }
 }
@@ -74,7 +84,7 @@ void TNRGLoader::parseEdges(ifstream & input, TransitNodeRoutingGraph & graph, u
 void TNRGLoader::parseRanks(ifstream & input, TransitNodeRoutingGraph & graph, unsigned int nodes) {
     unsigned int rank;
     for(unsigned int i = 0; i < nodes; i++) {
-        input >> rank;
+        input.read ((char*) &rank, sizeof(rank));
         graph.data(i).rank = rank;
     }
 }
@@ -83,7 +93,7 @@ void TNRGLoader::parseRanks(ifstream & input, TransitNodeRoutingGraph & graph, u
 void TNRGLoader::parseTransitNodesMapping(ifstream & input, TransitNodeRoutingGraph & graph, unsigned int tnodesAmount) {
     unsigned int originalID;
     for(unsigned int i = 0; i < tnodesAmount; i++) {
-        input >> originalID;
+        input.read ((char*) &originalID, sizeof(originalID));
         graph.addMappingPair(originalID, i);
     }
 }
@@ -93,7 +103,7 @@ void TNRGLoader::parseTransitNodesDistanceTable(ifstream & input, TransitNodeRou
     unsigned int value;
     for(unsigned int i = 0; i < tnodesAmount; i++) {
         for(unsigned int j = 0; j < tnodesAmount; j++) {
-            input >> value;
+            input.read ((char*) &value, sizeof(value));
             graph.setDistanceTableValue(i, j, value);
         }
     }
@@ -103,14 +113,16 @@ void TNRGLoader::parseTransitNodesDistanceTable(ifstream & input, TransitNodeRou
 void TNRGLoader::parseAccessNodes(ifstream & input, TransitNodeRoutingGraph & graph, unsigned int nodes) {
     unsigned int forwardNodes, backwardNodes, nodeID, nodeDistance;
     for(unsigned int i = 0; i < nodes; i++) {
-        input >> forwardNodes;
+        input.read ((char *) &forwardNodes, sizeof(forwardNodes));
         for(unsigned int j = 0; j < forwardNodes; j++) {
-            input >> nodeID >> nodeDistance;
+            input.read ((char *) &nodeID, sizeof(nodeID));
+            input.read ((char *) &nodeDistance, sizeof(nodeDistance));
             graph.addForwardAccessNode(i, nodeID, nodeDistance);
         }
-        input >> backwardNodes;
+        input.read ((char *) &backwardNodes, sizeof(backwardNodes));
         for(unsigned int j = 0; j < backwardNodes; j++) {
-            input >> nodeID >> nodeDistance;
+            input.read ((char *) &nodeID, sizeof(nodeID));
+            input.read ((char *) &nodeDistance, sizeof(nodeDistance));
             graph.addBackwardAccessNode(i, nodeID, nodeDistance);
         }
     }
@@ -121,7 +133,7 @@ void TNRGLoader::parseLocalityFilter(ifstream & input, TransitNodeRoutingGraph &
     bool localQueryFlag;
     for(unsigned int i = 0; i < nodes; i++) {
         for(unsigned int j = 0; j < nodes; j++) {
-            input >> localQueryFlag;
+            input.read ((char *) &localQueryFlag, sizeof(localQueryFlag));
             graph.setLocalityFilterValue(i, j, localQueryFlag);
         }
     }
