714b3c3e33eb9fa124f958e9cec3a302045e2557
Michal Cvach <michal.cvach@seznam.cz>
2020-04-29 02:17:42 +0200

    Removed some unused (and unnecessary) file from the 'src/API' subdirectory.

--- src/API/CHinterface.i
+++
@@ -1,5 +0,0 @@
-%module contractionHierarchies
-%include IntegerDistanceQueryManagerAPI.i
-%include IntegerDistanceQueryManagerWithMappingAPI.i
-%include FPointDistanceQueryManagerAPI.i
-%include FPointDistanceQueryManagerWithMappingAPI.i
--- src/API/generateSWIGcode.sh
+++ src/API/generateSWIGcode.sh
@@ -2,3 +2,7 @@
 echo "Generating SWIG code needed for creating the shared library..."
 swig -java -c++ -package cz.cvut.fel.aic.shortestpaths shortestPathsInterface.i
 echo "SWIG code generated!"
+
+# SIDENOTE: the package option in the swig call must match exactly
+# the package in which the classes will be put, otherwise it will
+# not be possible to call them from Java.
