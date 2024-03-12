583d6a345e347fc04a244158484a4bf2fb5e78aa
Jan Horák <horakj31@fel.cvut.cz>
2024-03-12 19:23:38 +0100

    CH functional tests

--- functest/common.cpp
+++ functest/common.cpp
@@ -4,6 +4,7 @@
 
 #include "gtest/gtest.h"
 #include "common.h"
+#include "GraphBuilding/Loaders/DDSGLoader.h"
 #include <fstream>
 #include <iterator>
 #include <string>
@@ -37,4 +38,49 @@ void compare_files(const std::string& p1, const std::string& p2) {
             std::istreambuf_iterator<char>(f2.rdbuf())
             ),
         true);
+}
+
+void compare_ch_files(const std::string& computed_fname, const std::string& expected_fname) {
+    DDSGLoader chLoader1 = DDSGLoader(computed_fname);
+    FlagsGraph* loaded = chLoader1.loadFlagsGraph();
+
+    DDSGLoader chLoader2 = DDSGLoader(expected_fname);
+    FlagsGraph* expected = chLoader2.loadFlagsGraph();
+
+    compare_flags_graphs(*loaded, *expected);
+}
+
+void compare_flags_graphs(
+    const FlagsGraph& computed,
+    const FlagsGraph& expected
+) {
+    ASSERT_EQ(computed.nodes(), expected.nodes());
+
+    const auto& computed_nodes_data = computed.getNodesData();
+    const auto& expected_nodes_data = expected.getNodesData();
+
+    for (unsigned int i = 0; i < computed_nodes_data.size(); ++i) {
+        ASSERT_EQ(computed_nodes_data[i].rank, expected_nodes_data[i].rank);
+        ASSERT_EQ(computed_nodes_data[i].backwardDist, expected_nodes_data[i].backwardDist);
+        ASSERT_EQ(computed_nodes_data[i].forwardDist, expected_nodes_data[i].forwardDist);
+        ASSERT_EQ(computed_nodes_data[i].backwardReached, expected_nodes_data[i].backwardReached);
+        ASSERT_EQ(computed_nodes_data[i].backwardSettled, expected_nodes_data[i].backwardSettled);
+        ASSERT_EQ(computed_nodes_data[i].backwardStalled, expected_nodes_data[i].backwardStalled);
+        ASSERT_EQ(computed_nodes_data[i].forwardReached, expected_nodes_data[i].forwardReached);
+        ASSERT_EQ(computed_nodes_data[i].forwardSettled, expected_nodes_data[i].forwardSettled);
+        ASSERT_EQ(computed_nodes_data[i].forwardStalled, expected_nodes_data[i].forwardStalled);
+    }
+
+    const auto& computed_neighbours = computed.getNeighbours();
+    const auto& expected_neighbours = expected.getNeighbours();
+
+    for (int i = 0; i < computed_neighbours.size(); i++) {
+        ASSERT_EQ(computed_neighbours[i].size(), expected_neighbours[i].size());
+        for (int j = 0; j < computed_neighbours[i].size(); j++) {
+            ASSERT_EQ(computed_neighbours[i][j].targetNode, expected_neighbours[i][j].targetNode);
+            ASSERT_EQ(computed_neighbours[i][j].weight, expected_neighbours[i][j].weight);
+            ASSERT_EQ(computed_neighbours[i][j].forward, expected_neighbours[i][j].forward);
+            ASSERT_EQ(computed_neighbours[i][j].backward, expected_neighbours[i][j].backward);
+        }
+    }
 }
--- functest/common.h
+++ functest/common.h
@@ -4,6 +4,12 @@
 
 #pragma once
 
+#include "GraphBuilding/Structures/FlagsGraph.h"
+
 void run_preprocessor(const char* args);
 
 void compare_files(const std::string& p1, const std::string& p2);
+
+void compare_ch_files(const std::string& computed_fname, const std::string& expected_fname);
+
+void compare_flags_graphs(const FlagsGraph& computed, const FlagsGraph& expected);
