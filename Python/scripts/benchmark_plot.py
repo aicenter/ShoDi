e698b6cd8de5f625e902ad83d86d89a5406072f2
Jan Horák <horakj31@fel.cvut.cz>
2024-09-12 11:35:19 +0200

    updated benchmarking scripts

--- Python/scripts/benchmark_plot.py
+++ Python/scripts/benchmark_plot.py
@@ -1,24 +1,29 @@
 import pandas
 import plotly.express as px
 
-df = pandas.read_csv('benchmark_data.csv')
+INSTANCE = "DC"
+LONG_NAME = "Washington, D. C."
+
+df = pandas.read_csv(f"benchmark_{INSTANCE}.csv")
 print(df)
 
-fig = px.scatter(df, x='memory', y='time', text='label', labels='', color='label', log_x=True, log_y=True, title="Memory and time comparison of various methods on the graph of Washington, D.C.")
+fig = px.scatter(
+    df, x="memory", y="time", text="label", labels="", log_x=True, log_y=True, template="simple_white",
+    title=f"Memory and time comparison of various methods on the graph of {LONG_NAME}")
 
-fig.update_traces(marker=dict(size=10, line=dict(width=2, color='DarkSlateGrey')))
-fig.update_xaxes(linecolor="black", mirror=True, title_text='Memory required by the structure in KB')
-fig.update_yaxes(linecolor="black", mirror=True, title_text='Average time for one query in μs')
-fig.update_traces(textposition='top center')
+fig.update_traces(marker=dict(size=10, color="powderblue", line=dict(width=2, color="DarkSlateGrey")))
+fig.update_xaxes(linecolor="black", mirror=True, title_text="Memory required by the structure in MiB", tickformat = ".1r")
+fig.update_yaxes(linecolor="black", mirror=True, title_text="Average time for one query in μs")
+fig.update_traces(textposition="bottom left")
 
 fig.update_layout(showlegend=False)
 fig.update_layout(
     title={
-        'x': 0.5,
-        'xanchor': 'center',
-        'y': 0.85,
-        'yanchor': 'top'
+        "x": 0.5,
+        "xanchor": "center",
+        "y": 0.85,
+        "yanchor": "top"
     }
 )
 
-fig.write_image("plot.png")
+fig.write_image(f"{INSTANCE}.png")
--- Python/scripts/benchmark_run.py
+++ Python/scripts/benchmark_run.py
@@ -2,54 +2,69 @@ import os
 import subprocess
 import pandas
 
-PREPROCESSOR_EXECUTABLE = './shortestPathsPreprocessor'
+PREPROCESSOR_EXECUTABLE = "./shortestPathsPreprocessor"
+RUNS = 10
+INSTANCE = "DC"
 
 def compute_structures():
-    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "create", "-m", "ch", "-i", "../thesisTestsData/Chicago", "-o", "../thesisTestsData/Chicago/Chicago"])
-    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "create", "-m", "tnr", "--preprocessing-mode", "slow", "--tnodes-cnt", "1000", "-i", "../thesisTestsData/Chicago", "-o", "../thesisTestsData/Chicago/Chicago1000tnodes"])
-    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "create", "-m", "tnraf", "--preprocessing-mode", "slow", "--tnodes-cnt", "1000", "-i", "../thesisTestsData/Chicago", "-o", "../thesisTestsData/Chicago/Chicago1000tnodes"])
-    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "create", "-m", "dm", "--preprocessing-mode", "fast", "-i", "../thesisTestsData/Chicago", "-o", "../thesisTestsData/Chicago/Chicago", "--output-format", "xdm"])
-    print('computing structures finished')
+    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "create", "-m", "ch", "-i", f"../thesisTestsData/{INSTANCE}", "-o", f"../thesisTestsData/{INSTANCE}/{INSTANCE}"])
+    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "create", "-m", "tnr", "--preprocessing-mode", "slow", "--tnodes-cnt", "5000", "-i", f"../thesisTestsData/{INSTANCE}", "-o", f"../thesisTestsData/{INSTANCE}/{INSTANCE}5000tnodes"])
+    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "create", "-m", "tnraf", "--preprocessing-mode", "slow", "--tnodes-cnt", "5000", "-i", f"../thesisTestsData/{INSTANCE}", "-o", f"../thesisTestsData/{INSTANCE}/{INSTANCE}5000tnodes"])
+    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "create", "-m", "dm", "--preprocessing-mode", "fast", "-i", f"../thesisTestsData/{INSTANCE}", "-o", f"../thesisTestsData/{INSTANCE}/{INSTANCE}", "--output-format", "hdf"])
+    print("computing structures finished")
 
+
+# returns memory usage for the method in MiB and average time per query in microseconds
 def run_benchmark(method, input_structure, queries_file):
-    command = [PREPROCESSOR_EXECUTABLE, "benchmark", "-m", method,
-        "--query-set", queries_file,
-        "--input-structure", input_structure,
-        "--output-path", f"out_{method}.txt"]
-    process = subprocess.Popen(command, preexec_fn=os.setsid)
-    print(' '.join(command))
-    process.communicate()
+    with open(queries_file, "r") as file:
+        # the first line of the file contains number of queries
+        queries_count = int(file.readline())
 
-    with open("benchmark.txt", "r") as f:
-        time, mem = f.readlines()
-    os.remove("benchmark.txt")
+    total_time = 0
+    total_mem = 0
+    for _ in range(RUNS):
+        command = [PREPROCESSOR_EXECUTABLE, "benchmark", "-m", method,
+            "--query-set", queries_file,
+            "--input-structure", input_structure,
+            "--output-path", f"out_{method}.txt"]
+        process = subprocess.Popen(command, preexec_fn=os.setsid)
+        print(" ".join(command))
+        process.communicate()
 
-    return mem.strip(), float(time)
+        with open("benchmark.txt", "r") as f:
+            time, mem = f.readlines()
+            total_time += float(time.strip())
+            total_mem += int(mem.strip())
+        os.remove("benchmark.txt")
+    
+    mem_mb = total_mem/(RUNS*1024)
+    query_time_us = (total_time*1_000_000)/(RUNS*queries_count)
+    return mem_mb, query_time_us
 
 compute_structures()
 
-queries = "../thesisTestsData/Chicago/Chicago100000randomQueries.txt"
+queries = f"../thesisTestsData/{INSTANCE}/{INSTANCE}100000randomQueries.txt"
 df_data = []
 
-mem, time = run_benchmark('dijkstra', '../thesisTestsData/Chicago', queries)
-df_data.append(['Dijkstra', mem, time])
+mem, time = run_benchmark("dijkstra", f"../thesisTestsData/{INSTANCE}", queries)
+df_data.append(["Dijkstra", mem, time])
 
-mem, time = run_benchmark('astar', '../thesisTestsData/Chicago', queries)
-df_data.append(['Astar', mem, time])
+mem, time = run_benchmark("astar", f"../thesisTestsData/{INSTANCE}", queries)
+df_data.append(["Astar", mem, time])
 
-mem, time = run_benchmark('ch', '../thesisTestsData/Chicago/Chicago.ch', queries)
-df_data.append(['CH', mem, time])
+mem, time = run_benchmark("ch", f"../thesisTestsData/{INSTANCE}/{INSTANCE}.ch", queries)
+df_data.append(["CH", mem, time])
 
-mem, time = run_benchmark('tnr', '../thesisTestsData/Chicago/Chicago1000tnodes.tnrg', queries)
-df_data.append(['TNR', mem, time])
+mem, time = run_benchmark("tnr", f"../thesisTestsData/{INSTANCE}/{INSTANCE}5000tnodes.tnrg", queries)
+df_data.append(["TNR", mem, time])
 
-mem, time = run_benchmark('tnraf', '../thesisTestsData/DC/DC1000tnodes.tgaf', queries)
-df_data.append(['TNRAF', mem, time])
+mem, time = run_benchmark("tnraf", f"../thesisTestsData/{INSTANCE}/{INSTANCE}5000tnodes.tgaf", queries)
+df_data.append(["TNRAF", mem, time])
 
-mem, time = run_benchmark('dm', '../thesisTestsData/DC/DC.xdm', queries)
-df_data.append(['DM', mem, time])
+mem, time = run_benchmark("dm", f"../thesisTestsData/{INSTANCE}/{INSTANCE}.hdf5", queries)
+df_data.append(["DM", mem, time])
 
-df = pandas.DataFrame(df_data, columns=['label', 'memory', 'time'])
+df = pandas.DataFrame(df_data, columns=["label", "memory", "time"])
 
 print(df)
-df.to_csv('benchmark_data.csv')
+df.to_csv(f"benchmark_{INSTANCE}.csv")
