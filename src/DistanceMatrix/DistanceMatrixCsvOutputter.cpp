5c80c763f0311b64a30105f653c1a8f8a4ed2b5c
Jan Horák <horakj31@fel.cvut.cz>
2024-09-17 16:23:54 +0200

    check available disk space when saving Distance Matrix

--- src/DistanceMatrix/DistanceMatrixCsvOutputter.cpp
+++ src/DistanceMatrix/DistanceMatrixCsvOutputter.cpp
@@ -6,20 +6,32 @@
 *****************************************************************************/
 
 #include <fstream>
+#include <filesystem>
 #include "DistanceMatrixCsvOutputter.h"
 #include "../constants.h"
 #include "../CLI/ProgressBar.hpp"
 
 void DistanceMatrixCsvOutputter::store(Distance_matrix_travel_time_provider &dm, const std::string &path) {
+    const auto& distances = dm.getRawData();
+    const auto nodesCnt = dm.nodes();
+
+    dist_t max_dist = 0;
+    for (size_t i = 0; i < static_cast<unsigned long long>(nodesCnt) * nodesCnt; i++) {
+        max_dist = std::max(max_dist, distances[i]);
+    }
+    unsigned long charsPerValue = std::to_string(max_dist).length();
+    unsigned long long totalChars = nodesCnt * (nodesCnt*charsPerValue + nodesCnt + 2);
+    std::filesystem::space_info si = std::filesystem::space(".");
+    if (totalChars > si.available) {
+        throw std::runtime_error("Not enough free disk space." + std::to_string(totalChars/1024) + "KiB required");
+    }
+
     printf("Storing the distance matrix.\n");
     std::ofstream output;
     output.open(path + ".csv");
     if (!output.is_open()) {
         printf("Couldn't open file '%s'!", (path + ".csv").c_str());
     }
-
-    const auto& distances = dm.getRawData();
-    const auto nodesCnt = dm.nodes();
 
     ProgressBar progress(nodesCnt);
     auto size = static_cast<size_t>(nodesCnt) * nodesCnt;
