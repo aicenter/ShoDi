6468eea2e484885195bd8fe856e5b346b608f07e
F.I.D.O. <david.fido.fiedler@gmail.com>
2025-05-16 18:47:35 +0200

    nb for generating queries updated; benchmark runner script updated

--- Python/scripts/benchmark_run.py
+++ Python/scripts/benchmark_run.py
@@ -23,22 +23,34 @@
 import os
 import subprocess
 import pandas
+from pathlib import Path
 
-PREPROCESSOR_EXECUTABLE = "./shortestPathsPreprocessor"
-BENCHMARK_EXECUTABLE = "./benchmark"
-RUNS = 10
-INSTANCE = "DC"
+instance_path = Path(r"C:\Google Drive\AIC Experiment Data\Shodi\Manhattan")
+
+# bin_folder = Path(".")
+bin_folder = Path(r"C:\Workspaces\AIC\shortest-distances\cmake-build-release")
+
+# queries_file_path = f"../thesisTestsData/{instance}/{instance}100000randomQueries.txt"
+queries_file_path = str(instance_path / "queries.txt")
+
+# output_path = Path(f"benchmark_{instance}.csv")
+output_path = instance_path / "output.csv"
+
+preprocessor_executable = str(bin_folder / "shortestPathsPreprocessor")
+benchmark_executable = str(bin_folder / "benchmark")
+runs = 1
+instance = "DC"
 
 def compute_structures():
-    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "-m", "ch", "-i", f"../thesisTestsData/{INSTANCE}", "-o", f"../thesisTestsData/{INSTANCE}/{INSTANCE}"])
-    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "-m", "tnr", "--preprocessing-mode", "slow", "--tnodes-cnt", "5000", "-i", f"../thesisTestsData/{INSTANCE}", "-o", f"../thesisTestsData/{INSTANCE}/{INSTANCE}5000tnodes", "--int-size", "16"])
-    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "-m", "tnraf", "--preprocessing-mode", "slow", "--tnodes-cnt", "5000", "-i", f"../thesisTestsData/{INSTANCE}", "-o", f"../thesisTestsData/{INSTANCE}/{INSTANCE}5000tnodes", "--int-size", "16"])
-    process = subprocess.run([PREPROCESSOR_EXECUTABLE, "-m", "dm", "--preprocessing-mode", "fast", "-i", f"../thesisTestsData/{INSTANCE}", "-o", f"../thesisTestsData/{INSTANCE}/{INSTANCE}", "--output-format", "hdf", "--int-size", "16"])
+    process = subprocess.run([preprocessor_executable, "-m", "ch", "-i", f"../thesisTestsData/{instance}", "-o", f"../thesisTestsData/{instance}/{instance}"])
+    process = subprocess.run([preprocessor_executable, "-m", "tnr", "--preprocessing-mode", "slow", "--tnodes-cnt", "5000", "-i", f"../thesisTestsData/{instance}", "-o", f"../thesisTestsData/{instance}/{instance}5000tnodes", "--int-size", "16"])
+    process = subprocess.run([preprocessor_executable, "-m", "tnraf", "--preprocessing-mode", "slow", "--tnodes-cnt", "5000", "-i", f"../thesisTestsData/{instance}", "-o", f"../thesisTestsData/{instance}/{instance}5000tnodes", "--int-size", "16"])
+    process = subprocess.run([preprocessor_executable, "-m", "dm", "--preprocessing-mode", "fast", "-i", f"../thesisTestsData/{instance}", "-o", f"../thesisTestsData/{instance}/{instance}", "--output-format", "hdf", "--int-size", "16"])
     print("computing structures finished")
 
 
 # returns memory usage for the method in MiB, average time per query in microseconds and a list of computed distances
-def run_benchmark(method, input_structure, queries_file, reference_distances=None):
+def run_benchmark(method, input_structure, queries_file, reference_distances=None, input_format=None):
     with open(queries_file, "r") as file:
         # the first line of the file contains number of queries
         queries_count = int(file.readline())
@@ -46,13 +58,25 @@ def run_benchmark(method, input_structure, queries_file, reference_distances=Non
     total_time = 0
     total_mem = 0
     out_values_set = set()
-    for i in range(RUNS):
+    for i in range(runs):
         out_file = f"out_{method}.txt"
-        command = [BENCHMARK_EXECUTABLE, "-m", method,
-            "--query-set", queries_file,
-            "--input-structure", input_structure,
-            "--output-path", out_file]
-        process = subprocess.Popen(command, preexec_fn=os.setsid)
+        command = [
+            benchmark_executable,
+            "-m",
+            method,
+            "--query-set",
+            queries_file,
+            "--input-structure",
+            input_structure,
+            "--output-path",
+            out_file
+        ]
+
+        # add input format if specified
+        if input_format is not None:
+            command.extend(["--input-format", input_format])
+
+        process = subprocess.Popen(command)
         print(" ".join(command), flush=True)
         process.communicate()
 
@@ -69,8 +93,8 @@ def run_benchmark(method, input_structure, queries_file, reference_distances=Non
         if len(out_values_set) > 1:
             print(f"WARNING: {method} returned different distances across multiple runs on the same query set!")
     
-    mem_mb = total_mem/(RUNS*1024)
-    query_time_us = (total_time*1_000_000)/(RUNS*queries_count)
+    mem_mb = total_mem/(runs * 1024)
+    query_time_us = (total_time*1_000_000)/(runs * queries_count)
     out_values = list(out_values_set)[0]
 
     if reference_distances is not None and out_values != reference_distances:
@@ -78,30 +102,39 @@ def run_benchmark(method, input_structure, queries_file, reference_distances=Non
 
     return mem_mb, query_time_us, out_values
 
-compute_structures()
 
-queries = f"../thesisTestsData/{INSTANCE}/{INSTANCE}100000randomQueries.txt"
+# compute_structures()
+
 df_data = []
 
+# # keep Dijkstra-computed distances and compare with other methods to confirm that the results are identic
+# mem, time, dijkstra_distances = run_benchmark("dijkstra", f"../thesisTestsData/{instance}", queries_file_path)
+# df_data.append(["Dijkstra", mem, time])
+#
+# mem, time, distances = run_benchmark("astar", f"../thesisTestsData/{instance}", queries_file_path, dijkstra_distances)
+# df_data.append(["A*", mem, time])
+#
+# mem, time, distances = run_benchmark("ch", f"../thesisTestsData/{instance}/{instance}.ch", queries_file_path, dijkstra_distances)
+# df_data.append(["CH", mem, time])
+#
+# mem, time, distances = run_benchmark("tnr", f"../thesisTestsData/{instance}/{instance}5000tnodes.tnrg", queries_file_path, dijkstra_distances)
+# df_data.append(["TNR", mem, time])
+#
+# mem, time, distances = run_benchmark("tnraf", f"../thesisTestsData/{instance}/{instance}5000tnodes.tgaf", queries_file_path, dijkstra_distances)
+# df_data.append(["TNRAF", mem, time])
+#
+# mem, time, distances = run_benchmark("dm", f"../thesisTestsData/{instance}/{instance}.hdf5", queries_file_path, dijkstra_distances)
+# df_data.append(["DM", mem, time])
+
+
 # keep Dijkstra-computed distances and compare with other methods to confirm that the results are identic
-mem, time, dijkstra_distances = run_benchmark("dijkstra", f"../thesisTestsData/{INSTANCE}", queries)
+mem, time, dijkstra_distances = run_benchmark("dijkstra", str(instance_path), queries_file_path, input_format="csv")
 df_data.append(["Dijkstra", mem, time])
 
-mem, time, distances = run_benchmark("astar", f"../thesisTestsData/{INSTANCE}", queries, dijkstra_distances)
-df_data.append(["A*", mem, time])
-
-mem, time, distances = run_benchmark("ch", f"../thesisTestsData/{INSTANCE}/{INSTANCE}.ch", queries, dijkstra_distances)
-df_data.append(["CH", mem, time])
-
-mem, time, distances = run_benchmark("tnr", f"../thesisTestsData/{INSTANCE}/{INSTANCE}5000tnodes.tnrg", queries, dijkstra_distances)
-df_data.append(["TNR", mem, time])
-
-mem, time, distances = run_benchmark("tnraf", f"../thesisTestsData/{INSTANCE}/{INSTANCE}5000tnodes.tgaf", queries, dijkstra_distances)
+mem, time, distances = run_benchmark("tnraf", str(instance_path / "ttmodel.tgaf"), queries_file_path, dijkstra_distances, input_format="csv")
 df_data.append(["TNRAF", mem, time])
-
-mem, time, distances = run_benchmark("dm", f"../thesisTestsData/{INSTANCE}/{INSTANCE}.hdf5", queries, dijkstra_distances)
-df_data.append(["DM", mem, time])
 
 df = pandas.DataFrame(df_data, columns=["label", "memory", "time"])
 
-df.to_csv(f"benchmark_{INSTANCE}.csv")
+df.to_csv(output_path)
+
